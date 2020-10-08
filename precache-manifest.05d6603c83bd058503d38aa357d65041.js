0097e2647a6ecff033aed273db62d973f0664f45
romerojoseing <romerojoseing@gmail.com>
2020-10-08 17:16:58 -0400

    Updates

---
+++ precache-manifest.05d6603c83bd058503d38aa357d65041.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1539f2f3466bd57695dbe318af2703",
+    "url": "/challenge-social-dashboard/index.html"
+  },
+  {
+    "revision": "f09592707fdcc07e8b53",
+    "url": "/challenge-social-dashboard/static/css/main.68d2156e.chunk.css"
+  },
+  {
+    "revision": "4165afecd7e9a7380c5b",
+    "url": "/challenge-social-dashboard/static/js/2.806a81fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/challenge-social-dashboard/static/js/2.806a81fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f09592707fdcc07e8b53",
+    "url": "/challenge-social-dashboard/static/js/main.5271c177.chunk.js"
+  },
+  {
+    "revision": "82e666bd1ec5005dad87",
+    "url": "/challenge-social-dashboard/static/js/runtime-main.9f098025.js"
+  }
+]);
